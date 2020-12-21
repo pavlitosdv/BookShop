3b141a41e6fb622f08ea72098acd25043b6f1b5b
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-12-21 20:29:27 +0200

    -Added Category controller into Area -> Admin area
    - Implemented Index and GetAll Action results methods
    
    - Index View Created for the Index of the Category controller
    
    - small modification to an <a> tag at the HTML _layout

---
+++ BookShop/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var getall = _unitOfWork.Category.GetAll();
+
+            return Json(new { data = getall });
+        }
+    }
+}
