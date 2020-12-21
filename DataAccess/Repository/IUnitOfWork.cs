5d22a225f2ef9b11f7c0fcc9aeb39047795e3168
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-12-21 17:34:44 +0200

    IUnitOfWork interface Added

---
+++ DataAccess/Repository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+
+        IStoreProcedure StoreProcedure { get; }
+    }
+}
