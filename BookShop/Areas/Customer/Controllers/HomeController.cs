d86795bd59bc2884b82214e2cf9f873abb121e4d
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-12-23 20:27:58 +0200

    typo correction

--- BookShop/Areas/Customer/Controllers/HomeController.cs
+++ BookShop/Areas/Customer/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace BookShop.Area.Customer.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoatingType");
             return View(productList);
         }
 
