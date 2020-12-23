3e20fe0b4b51331b1cbb7b9b21c994911100cc7e
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-12-23 20:23:26 +0200

    -added html content to the front page of the application
    - the customer's home controller, index action result modified to display the products to the front page

--- BookShop/Areas/Customer/Controllers/HomeController.cs
+++ BookShop/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using BookShop.Models.ViewModels;
+using DataAccess.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,15 +15,18 @@ namespace BookShop.Area.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
