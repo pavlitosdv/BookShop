6bd5caa7ec151168f4de10cfae82384ce909e0c7
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-01-03 20:33:13 +0200

    - Added 3 Classes: OrderDeatils, OrderHeader, ShoppingCart
    -DbSets added
    
    -migrations and update database performed

--- DataAccess/Migrations/20210103182736_Add_classes_ShoppingCart_OrderHeader_OrderDetails_to_DB.Designer.cs
+++ DataAccess/Migrations/20210103182736_Add_classes_ShoppingCart_OrderHeader_OrderDetails_to_DB.Designer.cs
@@ -4,14 +4,16 @@ using BookShop.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210103182736_Add_classes_ShoppingCart_OrderHeader_OrderDetails_to_DB")]
+    partial class Add_classes_ShoppingCart_OrderHeader_OrderDetails_to_DB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
