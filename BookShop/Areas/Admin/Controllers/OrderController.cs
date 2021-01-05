8a1e013cccc51394679709d5f0aa5124e7727b5b
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2021-01-05 18:51:52 +0200

    - Order controller created. and the Get order list controller added so that the orders to be diaplayed
    - Index view added for the order controller index action method
    
    - order javascripte script added

---
+++ BookShop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,84 @@
+﻿using DataAccess.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StoreProcedureCoverTypeConstants.Role_Admin)]
+    public class OrderController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        //[BindProperty]
+        //public OrderDetailsVM OrderVM { get; set; }
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetOrderList(string status)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            IEnumerable<OrderHeader> orderHeaderList;
+
+            if (User.IsInRole(StoreProcedureCoverTypeConstants.Role_Admin) || User.IsInRole(StoreProcedureCoverTypeConstants.Role_Employee))
+            {
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll(
+                                        u => u.ApplicationUserId == claim.Value,
+                                        includeProperties: "ApplicationUser");
+            }
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == StoreProcedureCoverTypeConstants.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == StoreProcedureCoverTypeConstants.StatusApproved ||
+                                                            o.OrderStatus == StoreProcedureCoverTypeConstants.StatusInProcess ||
+                                                            o.OrderStatus == StoreProcedureCoverTypeConstants.StatusPending);
+                    break;
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == StoreProcedureCoverTypeConstants.StatusShipped);
+                    break;
+                case "rejected":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == StoreProcedureCoverTypeConstants.StatusCancelled ||
+                                                            o.OrderStatus == StoreProcedureCoverTypeConstants.StatusRefunded ||
+                                                            o.OrderStatus == StoreProcedureCoverTypeConstants.PaymentStatusRejected);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaderList });
+        }
+        #endregion
+
+    }
+}
